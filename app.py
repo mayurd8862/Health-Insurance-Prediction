03efdc4afa6079b2a101999fd65deac5d0cbfa43
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2023-12-27 14:36:49 +0530

    data ingestion and dryrun

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 
 
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+# from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+# from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
+
+
+
 import sys
 
 
@@ -10,8 +17,8 @@ if __name__=="__main__":
 
     try:
         # #data_ingestion_config=DataIngestionConfig()
-        # data_ingestion=DataIngestion()
-        # train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         # #data_transformation_config=DataTransformationConfig()
         # data_transformation=DataTransformation()
@@ -21,7 +28,7 @@ if __name__=="__main__":
 
         # model_trainer=ModelTrainer()
         # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-        a = 1/00
+        # a = 1/00
         
     except Exception as e:
         logging.info("Custom Exception")
