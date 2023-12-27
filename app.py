80501ae3750d2a23bd3e410194881815c6699db5
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2023-12-27 15:04:31 +0530

    data transformation and save pkl file fn in utils

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from src.mlproject.logger import logging
 
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-# from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+
 # from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 
@@ -20,9 +22,9 @@ if __name__=="__main__":
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        # #data_transformation_config=DataTransformationConfig()
-        # data_transformation=DataTransformation()
-        # train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        # data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
         # ## Model Training
 
